4900e9f4fc9fb4d6e178f7a9361664fa58ca24c5
Nishant <nishantmallick02@gmail.com>
2026-07-23 16:51:18 +0530

    generated random marks for 500 students with 5 subjects

---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+rnd = np.random.default_rng(seed=10)
+
+min_mark = 0
+max_mark = 100
+no_of_students = 500
+no_of_subjects = 5
+
+student_marks = rnd.integers(min_mark,max_mark+1,size=(no_of_students,no_of_subjects))
+
+print(student_marks.shape)
+print(student_marks.dtype)
+print(student_marks[:5])

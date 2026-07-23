0b7ecb03c30207de7ec7b15a4a75b4c980851005
Nishant <nishantmallick02@gmail.com>
2026-07-23 18:34:25 +0530

    added functions for operations: giving the details about the topper, total average per subject

--- main.py
+++ main.py
@@ -6,7 +6,25 @@ min_mark = 0
 max_mark = 100
 no_of_students = 500
 no_of_subjects = 5
+subjects = ["Maths", "Physics", "Chemistry", "English", "Computer"]
 
-student_marks = rnd.integers(min_mark,max_mark+1,size=(no_of_students,no_of_subjects))
+student_marks = rnd.integers(
+    min_mark, max_mark + 1, size=(no_of_students, no_of_subjects)
+)
 average_marks = student_marks.mean(axis=1)
 subject_average = student_marks.mean(axis=0)
+topper_index = average_marks.argmax()
+
+
+def total_average_per_subject(arr):
+    for count, marks in enumerate(arr):
+        print(f"{subjects[count]} -> {marks}")
+
+
+def details_of_topper(subject_arr, marks_arr, avg_arr, topper_index):
+    print(
+        f"Student no. {topper_index+1} is the topper of the class.\nAverage -> {avg_arr[topper_index]}\n"
+    )
+
+    for subject, marks in zip(subject_arr, marks_arr[topper_index]):
+        print(f"{subject} -> {marks}")
